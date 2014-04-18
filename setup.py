d4fa4d19f272188e0ab660d614237b0ac8adc087
Christian Kohout <ck@getaweb.at>
2014-04-18 14:17:20 +0200

    fixed setup properties

--- setup.py
+++ setup.py
@@ -7,27 +7,22 @@ README = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
-    name='django-polls',
+    name='djangocms-getaweb-calendar',
     version='0.1',
-    packages=['polls'],
+    packages=['djangocms_calendar'],
     include_package_data=True,
-    license='BSD License',  # example license
-    description='A simple Django app to conduct Web-based polls.',
+    license='Unlicense',  # example license
+    description='A calendar app for Django CMS 3.0',
     long_description=README,
-    url='http://www.example.com/',
-    author='Your Name',
-    author_email='yourname@example.com',
+    url='https://github.com/kohout/djangocms-getaweb-calendar/',
+    author='Christian Kohout',
+    author_email='ck@getaweb.at',
     classifiers=[
         'Environment :: Web Environment',
         'Framework :: Django',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License', # example license
         'Operating System :: OS Independent',
         'Programming Language :: Python',
-        # Replace these appropriately if you are stuck on Python 2.
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.2',
-        'Programming Language :: Python :: 3.3',
         'Topic :: Internet :: WWW/HTTP',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
     ],
